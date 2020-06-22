3f31e766490dbf893de41bdffcb07414763fa2e8
Chase <chaseewhitney@gmail.com>
2020-06-22 12:33:01 -0500

    Register, Login, Authentication functional

--- MealTracker.API/Controllers/MealsController.cs
+++ MealTracker.API/Controllers/MealsController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MealTracker.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace MealTracker.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class MealsController : ControllerBase
