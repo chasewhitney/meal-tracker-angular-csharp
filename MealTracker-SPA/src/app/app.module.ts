99fe5b906de9811be4173fbb87e845495dd72289
Chase <chaseewhitney@gmail.com>
2020-06-22 14:25:01 -0500

    Register, Login, Logout working on client

--- MealTracker-SPA/src/app/app.module.ts
+++ MealTracker-SPA/src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
+import { AuthService } from './_services/auth.service';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from '../dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      DashboardComponent
+      HomeComponent,
+      NavComponent,
+      RegisterComponent
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule
    ],
-   providers: [],
+   providers: [
+      AuthService
+   ],
    bootstrap: [
       AppComponent
    ]
--- MealTracker-SPA/src/app/home/home.component.ts
+++ MealTracker-SPA/src/app/home/home.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class HomeComponent implements OnInit {
   baseUrl = 'http://localhost:5000/api/meals';
   meals: any = [];
+  registerMode = false;
 
   constructor(private http: HttpClient) { }
 
@@ -24,6 +25,14 @@ export class DashboardComponent implements OnInit {
     }, error => {
       console.log(error);
     });
+  }
+
+  registerToggle() {
+    this.registerMode = true;
+  }
+
+  cancelRegisterMode() {
+    this.registerMode = false;
   }
 
 }
